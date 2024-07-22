ca9b3141660a94f679de046abed66c67e965e739
sruedama <Sebastian.RuedaMarin@windriver.com>
2024-07-22 09:32:48 -0500

    Primera parte Menú

---
+++ menu/script.js
@@ -0,0 +1,18 @@
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('nav ul li a');
+
+    for (const link of links) {
+        link.addEventListener('click', (event) => {
+            event.preventDefault();
+
+            const targetId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetSection.offsetTop - document.querySelector('nav').offsetHeight,
+                behavior: 'smooth'
+            });
+        });
+    }
+});
